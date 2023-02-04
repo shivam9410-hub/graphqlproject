3f6599b7d2a4ac7b19f26ec0a5a5529461ede2d7
shivam kumar <shivamkhantwal9410@gmail.com>
2023-02-04 08:22:54 +0530

    modified db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,6 @@ const connectDB= async ()=>{
     const conn= await mongoose.connect("mongodb+srv://khantwal:12345@sandbox.vevuw.mongodb.net/graphql?retryWrites=true&w=majority") ;
     console.log(`MongoDB connected:${conn.connection.host}`) ;
 }
+console.log("shivam") ;
 module.exports=connectDB;
 
